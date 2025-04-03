025f3210c23c34dc76d9d80e07fb95ca14595465
01010101-coder <v.yahnavets@gmail.com>
2025-04-03 12:41:46 +0300

    commit

--- src/interface.h
+++ src/interface.h
@@ -3,33 +3,8 @@
 #define INTERFACE_H
 
 #include <sqlite3.h>
-
-typedef struct {
-    int helicopter_number;
-    char helicopter_model[128];
-    double total_earnings;
-    struct {
-        int tab_number;
-        char last_name[128];
-    } crew_members[10];
-    int crew_count;
-    struct {
-        char date[128];
-        int flight_code;
-        double flight_cost;
-    } flights[100];
-    int flight_count;
-} max_earning_crew_t;
-
-typedef struct {
-    int helicopter_number;
-    const char *model;
-    const char *manufacture_date;
-    double max_payload;
-    const char *last_repair_date;
-    int flight_resource;
-    int found;
-} HelicopterInfo;
+#include "crew.h"
+#include "commander.h"
 
 void commander_interface(sqlite3 *db);
 void crew_member_interface(sqlite3 *db, int tab_number);
